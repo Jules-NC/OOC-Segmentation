e21436d44be08ba3a451c187f2e326a4a1b885df
Unknown <stelaespindola@gmail.com>
2019-02-11 19:53:43 +0100

    Just new outputs

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ sys.setrecursionlimit(15000)
 IMAGE.len_x = 4
 IMAGE.len_y = 2
 
-server2 = Server(IMAGE.len_x, IMAGE.len_y, 2, 1, "Teste1")
+server2 = Server(IMAGE.len_x, IMAGE.len_y, 2, 1, "Test2")
 
-weights = [1, 2, 2, 0, 3, 1, 0, 4, 0, 3]
+weights = [1, 1, 2, 0, 2, 1, 0, 3, 0, 3]
 
 server2.initiate(weights)
