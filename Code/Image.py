597b2318f9b88b9666a5a9724e7e3c950031207b
thesylio <alexandre.durmeyer@edu.esiee.fr>
2019-04-18 14:16:24 +0200

    Update Image.py

--- Code/Image.py
+++ Code/Image.py
@@ -2,7 +2,7 @@ import collections
 
 ImageSize = collections.namedtuple('ImageSize', ['len_x', 'len_y'])
 
-
+""" An image just store its size """
 class IMAGE:
     def __init__(self, x, y):
         self.len_x = x
